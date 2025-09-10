3922d366f6afd375d6517b34183a7239838e101f
kyjhcw-rgb <kyjhcw@gmail.com>
2025-09-10 23:20:13 +0900

    객체지향프로그램 2주차 실습

---
+++ java/src/ch01/sec08/Hello.java
@@ -0,0 +1,10 @@
+package ch01.sec08;	//src안에 있고, pacakge안에 class가 있음(class는 객체를 만들기 위한 틀), class 안에 메소드가 있음 
+
+public class Hello {
+	/*public 공용, static 프로그램 끝날 때까지 유지)*/
+	public static void main(String[] args) {	//메인 메소드
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
---
+++ java/src/ch01/sec10/Calculator.java
@@ -0,0 +1,12 @@
+package ch01.sec10;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		int x = 1;	//리터럴 상수 1을 변수 x(식별자)에 정수로 저장
+		int y = 2;
+		int result = x + y;
+		System.out.println(result);
+	}
+
+}
